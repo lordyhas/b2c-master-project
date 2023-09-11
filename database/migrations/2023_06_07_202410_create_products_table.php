4b40c96681481cabb7b10d9f71fa3a3831e24182
lordyhas <dev.haspro@gmail.com>
2023-09-11 15:19:30 +0200

    create migrations of product and transaction | version 0.0.1+2

---
+++ database/migrations/2023_06_07_202410_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('model', 20);
+            $table->decimal('purchasePrice', 10, 2);
+            $table->decimal('salePrice', 10, 2);
+            $table->text('description')->nullable();
+            $table->string('productType', 20);
+            $table->string('employee', 50);
+            $table->decimal('promotionalPrice', 10, 2)->nullable();
+            $table->date('promotionalOutdated')->nullable();
+            $table->integer('stock');
+            $table->integer('threshold');
+            $table->string('images', 255)->nullable();
+            $table->string('address', 100);
+            $table->boolean('canReserve')->default(false);
+            $table->boolean('isTendency')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
---
+++ database/migrations/2023_06_07_202534_create_transactions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->date('purchaseDate');
+            $table->foreignId('productId')->constrained('products');
+            $table->foreignId('customerId')->constrained('customers');
+            $table->integer('quantity');
+            $table->decimal('amount', 10, 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
