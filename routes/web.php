78026dd411229d890ace702ba4eed64b616ab0be
lordyhas <dev.haspro@gmail.com>
2023-09-12 16:38:59 +0200

    breeze auth - routes | version 0.0.1+3

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,55 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+
+Route::prefix('/x/blog')->name('blog.')
+    ->group(function () {
+
+        Route::get('/', function (Request $request) {
+            return [
+                "id" => $request->input("id", "0"),
+                "title" => "Nom titre",
+                "contents" => "Bonjour contents",
+            ];
+        })->name("index");
+
+        Route::get('/{title}-{id}', function (string $title, string $id) {
+            return [
+                "id" => $id,
+                "title" => $title,
+                "contents" => "Bonjour contents",
+            ];
+        })->where([
+            'id' => '[0-9]+',
+            'title' => '[a-z0-9\-]+',
+        ])->name("compose ");
+
+    });
+
+Route::get('/produits/{name}-{no}',
+    function (string $name, string $no, Request $request) {
+        return [
+            "id" => $request->input("id", "0"),
+            "product_name" => $name,
+            "pno" => $no,
+            "contents" => "Bonjour contents",
+        ];
+    })->where([
+    'no' => '[0-9]+',
+    'name' => '[a-z0-9\-]+',
+])->name("ventes");
+
+
+require __DIR__.'/auth.php';
