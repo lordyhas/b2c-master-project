3a684dfa2e36f345fa236f962d0dcd5ca11643e1
lordyhas <dev.haspro@gmail.com>
2023-09-15 20:54:17 +0200

    ide helper added | 0.0.1+4

---
+++ _ide_helper_models.php
@@ -0,0 +1,165 @@
+<?php
+
+// @formatter:off
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models{
+/**
+ * App\Models\Customer
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $phoneNumber
+ * @property string $email
+ * @property int $locationId
+ * @property string|null $address
+ * @method static \Database\Factories\CustomerFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereAddress($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereLocationId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer wherePhoneNumber($value)
+ * @mixin \Eloquent
+ */
+	class IdeHelperCustomer {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Location
+ *
+ * @property int $id
+ * @property string $town
+ * @property string $province
+ * @property string $country
+ * @method static \Database\Factories\LocationFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Location newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Location newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Location query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Location whereCountry($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Location whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Location whereProvince($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Location whereTown($value)
+ * @mixin \Eloquent
+ */
+	class IdeHelperLocation {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Product
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $model
+ * @property string $purchasePrice
+ * @property string $salePrice
+ * @property string|null $description
+ * @property string $productType
+ * @property int $employeeId
+ * @property string|null $promotionalPrice
+ * @property string|null $promotionalOutdated
+ * @property int $stock
+ * @property int $threshold
+ * @property string|null $images
+ * @property string $address
+ * @property int $canReserve
+ * @property int $isTendency
+ * @method static \Database\Factories\ProductFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Product newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Product newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Product query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereAddress($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereCanReserve($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereEmployeeId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereImages($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereIsTendency($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereModel($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereProductType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product wherePromotionalOutdated($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product wherePromotionalPrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product wherePurchasePrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereSalePrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereStock($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereThreshold($value)
+ * @mixin \Eloquent
+ */
+	class IdeHelperProduct {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Transaction
+ *
+ * @property int $id
+ * @property string $purchaseDate
+ * @property int $productId
+ * @property int $customerId
+ * @property int $quantity
+ * @property string $amount
+ * @method static \Database\Factories\TransactionFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Transaction newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Transaction newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Transaction query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Transaction whereAmount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Transaction whereCustomerId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Transaction whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Transaction whereProductId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Transaction wherePurchaseDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Transaction whereQuantity($value)
+ * @mixin \Eloquent
+ */
+	class IdeHelperTransaction {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string|null $phone
+ * @property string|null $reg_no
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property mixed $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \Laravel\Sanctum\PersonalAccessToken> $tokens
+ * @property-read int|null $tokens_count
+ * @method static \Database\Factories\UserFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRegNo($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+	class IdeHelperUser {}
+}
+
