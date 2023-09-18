7bce9632fea6cfad7b1a0318c3e05eab67da82ed
lordyhas <dev.haspro@gmail.com>
2023-09-19 00:40:30 +0200

    migration add column on products | 0.0.1+4

---
+++ database/migrations/2023_09_18_221450_add_at_column_from_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->date('updated_at');
+            $table->date('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            if (Schema::hasColumn('customers', 'updated_at') &&
+                Schema::hasColumn('customers', 'created_at')) {
+                // drop the phone column
+
+                $table->dropColumn('updated_at');
+                $table->dropColumn('created_at');
+            }
+        });
+    }
+};
