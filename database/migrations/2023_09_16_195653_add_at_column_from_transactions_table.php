ee50a29a234232dfd557e2e5083046e395fae542
lordyhas <dev.haspro@gmail.com>
2023-09-18 12:37:26 +0200

    migration add column on transactions | 0.0.1+4

---
+++ database/migrations/2023_09_16_195653_add_at_column_from_transactions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->date('updated_at');
+            $table->date('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            if (Schema::hasColumn('transactions', 'updated_at') &&
+                Schema::hasColumn('transactions', 'created_at')) {
+                // drop the phone column
+
+                $table->dropColumn('updated_at');
+                $table->dropColumn('created_at');
+            }
+        });
+    }
+};
