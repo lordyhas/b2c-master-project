6eb904ac2c1f4390bfcf19c656a6f2a1501745b4
lordyhas <dev.haspro@gmail.com>
2023-09-26 00:47:05 +0200

    version 0.0.1+9

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -8,8 +8,8 @@
     <div class="py-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in! (add text ...)") }}
+                <div class="p-6 text-gray-900">
+                    {{ __("You're logged in! ") }}
                 </div>
             </div>
         </div>
@@ -21,10 +21,9 @@
                 <div class="p-6">
                     <h3 class="text-xl"><b>{{ __("Les produits enregistrés") }}</b></h3>
                     <br>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni quos esse tenetur illo
-                        qui, nostrum ipsum dolor sit amet, consectetur adipisicing elit. Magni quos esse tenetur illo
-                    </p>
+                    <p>Le nombre de produit : -- </p>
+                    <p>Les produits en rupture des stocks : -- </p>
+                    <p>Les produits bientôt en rupture  : -- </p>
                     <div> </div>
                 </div>
             </div>
--- resources/views/customer.blade.php
+++ resources/views/customer.blade.php
@@ -4,9 +4,18 @@
             {{ __("Nos clients") }}
         </h2>
     </x-slot>
+    <div class="py-6">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <h3 class="text-xl"><b>{{ __("") }}</b></h3>
+                </div>
+            </div>
+        </div>
+    </div>
 
     @if(true)
-        <div class="py-1">
+        <div class="py-6">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6">
