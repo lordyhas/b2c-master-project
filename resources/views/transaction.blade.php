9a9bb4578ea6b2f11b930dd1364fa607a5e3899b
lordyhas <dev.haspro@gmail.com>
2023-09-26 00:41:12 +0200

    version 0.0.1+9

--- resources/views/transaction.blade.php
+++ resources/views/transaction.blade.php
@@ -9,7 +9,7 @@
         <div class="py-6">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <div class="p-6">
                         <h3 class="mb-3  text-xl"><b>{{ __("Les produits enregistrés") }}</b></h3>
 
                         <div>
