6e2fe1d364114ff2ea47463f7ea27191d5860e4f
lordyhas <dev.haspro@gmail.com>
2023-09-26 00:53:52 +0200

    version 0.0.1+9

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -21,8 +21,25 @@
                 <div class="p-6">
                     <h3 class="text-xl"><b>{{ __("Les produits enregistrés") }}</b></h3>
                     <br>
-                    <p>Le nombre de produit : -- </p>
+                    <p>Le nombre des produits : -- </p>
                     <p>Les produits en rupture des stocks : -- </p>
+                    <p>Les produits bientôt en rupture  : -- </p>
+                    <div> </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="py-1">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6">
+                    <h3 class="text-xl"><b>{{ __("Les clients enregistrés") }}</b></h3>
+                    <br>
+                    <p>Le client du mois : -- </p>
+                    <p>Le nombre des clients : -- </p>
+                    <p>Le nombre des clients mois ({{date('m/Y', time())}}) : -- </p>
+                    <p>Le nombre des cartes fidélité : -- </p>
                     <p>Les produits bientôt en rupture  : -- </p>
                     <div> </div>
                 </div>
